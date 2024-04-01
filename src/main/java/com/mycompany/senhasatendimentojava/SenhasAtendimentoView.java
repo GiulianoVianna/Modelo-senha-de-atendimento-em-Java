f3b1e36c86184b05f95dc3d310f20985224c67e8
GiulianoVianna <giulianoviannasilva@gmail.com>
2024-04-01 10:26:01 -0400

    Atualização projeto

--- src/main/java/com/mycompany/senhasatendimentojava/SenhasAtendimentoView.java
+++ src/main/java/com/mycompany/senhasatendimentojava/SenhasAtendimentoView.java
@@ -61,6 +61,11 @@ public class SenhasAtendimentoView extends javax.swing.JFrame {
         jButton1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jButton1.setForeground(new java.awt.Color(255, 255, 255));
         jButton1.setText("Atendimento Prioritário");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jButton2.setForeground(new java.awt.Color(255, 255, 255));
@@ -114,6 +119,10 @@ public class SenhasAtendimentoView extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments

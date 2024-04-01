c3e57f3e5c29c41498438fb547e2bc9a365e0733
GiulianoVianna <giulianoviannasilva@gmail.com>
2024-04-01 09:50:32 -0400

    Atualização projeto

---
+++ src/main/java/com/mycompany/senhasatendimentojava/SenhasAtendimentoJava.java
@@ -0,0 +1,18 @@
+
+
+package com.mycompany.senhasatendimentojava;
+
+/**
+ *
+ * @author Giuliano Vianna
+ */
+public class SenhasAtendimentoJava {
+
+    public static void main(String[] args) {
+        
+        SenhasAtendimentoView objSenhasAtendimento = new SenhasAtendimentoView();
+        
+        objSenhasAtendimento.setLocationRelativeTo(null);
+        objSenhasAtendimento.setVisible(true);
+    }
+}
